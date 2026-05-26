677454bdd0cc19d9bdb09c7de52b6e1c110c54d4
johnmullan <johnmullan1408@gmail.com>
2026-05-26 23:14:42 +0000

    Added sidebar components

---
+++ frontend/src/data/tracks.ts
@@ -0,0 +1,23 @@
+export const tracks = [
+    {
+        name: "CCNA",
+        subtitle: "200-301",
+        href: "/ccna",
+        count: "filler",
+        status: "live",
+    },
+    {
+        name: "CCNP",
+        subtitle: "ENCOR 350-401",
+        href: "/ccnp-encor",
+        count: "filler",
+        status: "live",
+    },
+    {
+        name: "Labs",
+        subtitle: "filler",
+        href: null,
+        count: "filler",
+        status: "soon",
+    },
+] as const;
